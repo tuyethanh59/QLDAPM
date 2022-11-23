abf674b2c24e6c1b5992de579605123ad2b5bdaa
unknown <nnlanh_20th@student.agu.edu.vn>
2022-11-23 15:08:19 +0700

    LA

--- admin/ktnguoidung/index.php
+++ admin/ktnguoidung/index.php
@@ -1,31 +1,29 @@
-<?php 
+<?php
 require("../../model/database.php");
 require("../../model/nguoidung.php");
 
 // Biến cho biết ng dùng đăng nhập chưa
 $isLogin = isset($_SESSION["nguoidung"]);
 
-if(isset($_REQUEST["action"])){
+if (isset($_REQUEST["action"])) {
     $action = $_REQUEST["action"];
-}
-elseif($isLogin == FALSE){
+} elseif ($isLogin == FALSE) {
     $action = "dangnhap";
-}
-else{
-    $action="macdinh"; 
+} else {
+    $action = "macdinh";
 }
 
 $nguoidung = new NGUOIDUNG();
 $tb = "";
-switch($action){
-    case "macdinh":   
-             
+switch ($action) {
+    case "macdinh":
+
         include("main.php");
         break;
     case "dangxuat":
-        
+
         unset($_SESSION["nguoidung"]);
-                
+
         $tb = "Cảm ơn!";
         //SignIn.php
         //loginform.php
@@ -37,11 +35,10 @@ switch($action){
     case "xldangnhap":
         $email = $_POST["your_name"];
         $matkhau = $_POST["your_pass"];
-        if($nguoidung->kiemtranguoidunghople($email,$matkhau)==TRUE){
+        if ($nguoidung->kiemtranguoidunghople($email, $matkhau) == TRUE) {
             $_SESSION["nguoidung"] = $nguoidung->laythongtinnguoidung($email);
             include("main.php");
-        }
-        else{
+        } else {
             $tb = "Đăng nhập không thành công!";
             include("SignIn.php");
         }
@@ -50,25 +47,40 @@ switch($action){
     case "capnhaths":
         $mand = $_POST["txtid"];
         $email = $_POST["your_name"];
-        
+
         $sodt = $_POST["txtdienthoai"];
         $hoten = $_POST["txthoten"];
         $hinhanh = basename($_FILES["fhinh"]["name"]);
         $duongdan = "../images/" . $hinhanh;
         move_uploaded_file($_FILES["fhinh"]["tmp_name"], $duongdan);
-        
-        $nguoidung->capnhatnguoidung($mand,$email,$sodt,$hoten,$hinhanh);
+
+        $nguoidung->capnhatnguoidung($mand, $email, $sodt, $hoten, $hinhanh);
 
         $_SESSION["nguoidung"] = $nguoidung->laythongtinnguoidung($email);
-        include("main.php");        
+        include("main.php");
         break;
 
     case "doimatkhau":
-         if (isset($_POST["your_name"]) && isset($_POST["txtmatkhaumoi"]) )
-            $nguoidung->doimatkhau($_POST["your_name"],$_POST["txtmatkhaumoi"]);
+        if (isset($_POST["your_name"]) && isset($_POST["txtmatkhaumoi"]))
+            $nguoidung->doimatkhau($_POST["your_name"], $_POST["txtmatkhaumoi"]);
         include("main.php");
-        break;    
+        break;
     default:
         break;
 }
 ?>
+
+<body onload="setVolume();">
+    <audio id="audi" autoplay loop>
+        <source src="../../images/h.mp3" type="audio/mpeg">
+    </audio>
+
+
+    <script type="text/javascript">
+        var au = document.getElementById("audi");
+
+        function setVolume() {
+            au.volume = 100000.3;
+        }
+    </script>
+</body>
--- view/top.php
+++ view/top.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <title>It Dream - Cửa hàng buôn bán họa cụ </title>
   <meta charset="utf-8">
@@ -10,86 +11,99 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <style>
-  h3{
-    text-shadow: 2px 2px 2px silver;
-  }
-  .carousel-inner img {  
-      width: 100%; /* Set width to 100% */
-      margin: auto;
-  }
-  .carousel-caption h3 {
-      color: #fff !important;
-  }
-  @media (max-width: 600px) {
-    .carousel-caption {
-      display: none; /* Hide the carousel text when the screen is less than 600 pixels wide */
+    h3 {
+      text-shadow: 2px 2px 2px silver;
     }
-  }
-  footer {
+
+    .carousel-inner img {
+      width: 100%;
+      /* Set width to 100% */
+      margin: auto;
+    }
+
+    .carousel-caption h3 {
+      color: #fff !important;
+    }
+
+    @media (max-width: 600px) {
+      .carousel-caption {
+        display: none;
+        /* Hide the carousel text when the screen is less than 600 pixels wide */
+      }
+    }
+
+    footer {
       background-color: black;
       color: #f5f5f5;
       padding: 32px;
-  }
-  footer a:hover {
+    }
+
+    footer a:hover {
       color: #777;
       text-decoration: none;
-  }  
+    }
+
+    .navbar-inverse {
+      background-color: red;
+
+    }
   </style>
 </head>
+
 <body id="abc" data-spy="scroll" data-target=".navbar" data-offset="50">
 
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-     
-      <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-home"></span> Dream Shop</a>
-    </div>
-    
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-     
-        <li class="nav-item dropdown">
-          <a class="nav-link" href="#" data-toggle="dropdown">
-          <span class="glyphicon glyphicon-th"></span> Danh mục sản phẩm</a>
-          </a>
-          
-          <ul class="dropdown-menu">
-            <?php            
-            foreach($danhmuc as $dm):
-            ?>
-            <li><a href="?action=xemnhom&madm=<?php echo $dm["id"]; ?>"><?php echo $dm["tendanhmuc"]; ?></a></li>
-            <?php endforeach; ?>
-          </ul>
-        </li>
-    
-        <li><a href="#"><span class="glyphicon glyphicon-phone-alt"></span> Liên hệ</a></li>
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
 
-        <li><a href="#" data-toggle="modal" data-target="#modalTimKiem">
-          <span class="glyphicon glyphicon-search"></span> Tìm kiếm</a></li>  
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-      <li><a href="admin/ktnguoidung/index.php"><span class="glyphicon glyphicon-user"></span> Đăng nhập</a></li>
-        <li><a href="admin/ktnguoidung/SignUp.php"><span class="glyphicon glyphicon-user"></span> Đăng ký</a></li>
-        <li><a href="#" class="text-warning">
-          <span class="glyphicon glyphicon-shopping-cart"></span> Giỏ hàng
-          <span class="badge"><?php echo demhangtronggio(); ?></span>
-           </a>
-        </li>             
-      </ul>
+        <a class="navbar-brand" href="index.php"><span class="glyphicon glyphicon-home"></span> Dream Shop</a>
+      </div>
+
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+
+          <li class="nav-item dropdown">
+            <a class="nav-link" href="#" data-toggle="dropdown">
+              <span class="glyphicon glyphicon-th"></span> Danh mục sản phẩm</a>
+            </a>
+
+            <ul class="dropdown-menu">
+              <?php
+              foreach ($danhmuc as $dm) :
+              ?>
+                <li><a href="?action=xemnhom&madm=<?php echo $dm["id"]; ?>"><?php echo $dm["tendanhmuc"]; ?></a></li>
+              <?php endforeach; ?>
+            </ul>
+          </li>
+
+          <li><a href="#"><span class="glyphicon glyphicon-phone-alt"></span> Liên hệ</a></li>
+
+          <li><a href="#" data-toggle="modal" data-target="#modalTimKiem">
+              <span class="glyphicon glyphicon-search"></span> Tìm kiếm</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="admin/ktnguoidung/index.php"><span class="glyphicon glyphicon-user"></span> Đăng nhập</a></li>
+          <li><a href="admin/ktnguoidung/SignUp.php"><span class="glyphicon glyphicon-user"></span> Đăng ký</a></li>
+          <li><a href="#" class="text-warning">
+              <span class="glyphicon glyphicon-shopping-cart"></span> Giỏ hàng
+              <span class="badge"><?php echo demhangtronggio(); ?></span>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
   <!-- Hộp tìm kiếm -->
   <div class="modal fade" id="modalTimKiem" role="dialog">
     <div class="modal-dialog">
-    
+
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
@@ -109,14 +123,14 @@
                 <option value="baiviet">Tin tức</option>
               </select>
             </div>
-              <button type="submit" class="btn btn-info">Tìm kiếm  
-                <span class="glyphicon glyphicon-ok"></span>
-              </button>
+            <button type="submit" class="btn btn-info">Tìm kiếm
+              <span class="glyphicon glyphicon-ok"></span>
+            </button>
           </form>
         </div>
-        
+
       </div>
     </div>
   </div>
 
-<br>
+  <br>
